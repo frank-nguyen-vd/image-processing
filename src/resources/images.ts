c8da5560b769035d8d6511b46bacf5e07c237f2b
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-29 23:25:55 +0800

    :sparkles: REturn a list of images if no query params

--- src/resources/images.ts
+++ src/resources/images.ts
@@ -1,13 +1,33 @@
 import express from 'express';
 import log4js from 'log4js';
+import { promises as fs } from 'fs';
 
 const log = log4js.getLogger('/images');
+
+const assetsDir = './assets/original';
 
 const imagesController = express.Router();
 imagesController.get('/', (req: express.Request, res: express.Response) => {
     log.debug('/images is called');
-    return res.send({
-        success: true
+
+    // TODO: Handle scenario which folder `assets` does not exist
+    // TODO: Return only one image that matches given `filename`
+    // TODO: Resize the requested image to the given dimensions
+    fs.readdir(assetsDir).then((files: string[]) => {
+        if (!req.query.filename && !req.query.width && !req.query.height) {
+            const images = files.map((file) => {
+                return { filename: file };
+            });
+
+            return res.status(200).json({
+                success: true,
+                images: images
+            });
+        } else {
+            return res.status(400).json({
+                success: false
+            });
+        }
     });
 });
 
